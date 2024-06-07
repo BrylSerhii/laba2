9ec4e4c6091bf4df7bc36cd2a221b8d47ea33c93
BrylSerhii <iggrooot@gmail.com>
2024-06-07 21:11:43 +0300

    added factory pattern

---
+++ laba2/SortAlgorithmFactory.cs
@@ -0,0 +1,30 @@
+﻿using laba3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2
+{
+    public abstract class SortAlgorithmFactory
+    {
+        public abstract ISortAlgorithm CreateSortAlgorithm();
+    }
+
+    public class SequentialMergeSortFactory : SortAlgorithmFactory
+    {
+        public override ISortAlgorithm CreateSortAlgorithm()
+        {
+            return new SequentialMergeSort();
+        }
+    }
+
+    public class ConcurrentMergeSortFactory : SortAlgorithmFactory
+    {
+        public override ISortAlgorithm CreateSortAlgorithm()
+        {
+            return new ConcurrentMergeSort();
+        }
+    }
+}
