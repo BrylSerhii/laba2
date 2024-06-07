b0f51b9db30ec08d6760b1bfb1784b83173e1527
BrylSerhii <iggrooot@gmail.com>
2024-06-07 21:13:22 +0300

    added strategy pattern

---
+++ laba2/SortContext.cs
@@ -0,0 +1,24 @@
+﻿using laba3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2
+{
+    public class SortContext
+    {
+        private ISortAlgorithm _sortAlgorithm;
+
+        public void SetSortAlgorithm(ISortAlgorithm sortAlgorithm)
+        {
+            _sortAlgorithm = sortAlgorithm;
+        }
+
+        public void Sort(int[] array)
+        {
+            _sortAlgorithm.Sort(array);
+        }
+    }
+}
