8f22f1e89e3ffdba82a9d1f5fe7071e572020b49
BrylSerhii <iggrooot@gmail.com>
2024-06-07 21:12:18 +0300

    added facade pattern

---
+++ laba2/SortingFacade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2
+{
+    public class SortingFacade
+    {
+        private readonly SortAlgorithmFactory _sequentialFactory;
+        private readonly SortAlgorithmFactory _concurrentFactory;
+
+        public SortingFacade()
+        {
+            _sequentialFactory = new SequentialMergeSortFactory();
+            _concurrentFactory = new ConcurrentMergeSortFactory();
+        }
+
+        public void SortSequentially(int[] array)
+        {
+            var sorter = _sequentialFactory.CreateSortAlgorithm();
+            sorter.Sort(array);
+        }
+
+        public void SortConcurrently(int[] array)
+        {
+            var sorter = _concurrentFactory.CreateSortAlgorithm();
+            sorter.Sort(array);
+        }
+    }
+}
